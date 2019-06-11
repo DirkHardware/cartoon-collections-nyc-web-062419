1e512a1142b51d5ad9c94da239d5665e1b09cfbe
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-11 21:36:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,8 +22,9 @@ def summon_captain_planet(planateers)
   planateers
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planateers)
+  planateers.map! do |call|
+    call.length > 4
 end
 
 def find_the_cheese# code an argument here
