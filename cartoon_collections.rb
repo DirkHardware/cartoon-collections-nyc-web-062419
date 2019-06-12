b1a03c01143d6a0090a8389a7ce0fe6aa7602507
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-12 01:49:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,8 +31,6 @@ end
 def find_the_cheese(platter)
   cheese_types = ["cheddar", "gouda", "camembert"]
   platter.find do |selection|
-    selection.include?(cheese_types[0])
-    selection.include?(cheese_types[1])
-    selection.include?(cheese_types[2])
+    selection.include?("cheddar")
   end
 end
